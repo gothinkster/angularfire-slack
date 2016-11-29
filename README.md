de92a69bb9eb6d8ad1f0b7c506e09ba23078e45a
Albert Pai <albert@pai.email>
2016-11-29 09:35:08 -0800

    Initial Code

---
+++ README.md
@@ -0,0 +1,17 @@
+# angularfire-slack
+
+This repository includes the source code for getting started with the [Build a Real-Time Slack Clone using AngularFire](https://thinkster.io/tutorials/angularfire-realtime-slack-clone)
+made by [Thinkster](https://thinkster.io)
+
+***
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.11.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularfireSlackApp
+ * @description
+ * # angularfireSlackApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularfireSlackApp', [
+    'firebase',
+    'angular-md5',
+    'ui.router'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home/home.html'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'auth/login.html'
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'auth/register.html'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
